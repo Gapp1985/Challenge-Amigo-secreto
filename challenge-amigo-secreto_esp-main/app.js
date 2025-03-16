c7d4ac624c4806b119c9217d1cef0cf71497d92a
Gapp1985 <giancarlo.payhua@gmail.com>
2025-03-15 23:06:39 -0300

    Add files via upload
    Iniciando el challenge

---
+++ challenge-amigo-secreto_esp-main/app.js
@@ -0,0 +1,8 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// Declarando variables, reusando nombres del .html
+let listaAmigos = [];
+let inputAmigo = document.getElementById("amigo");
+let amigo = ();
+let resultado = ();
+
+
